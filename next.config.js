e419a8cfb63dedcbb65001425acaaf8445107acb
Yuriy Goryakin <yura@Yuriys-MacBook-Air.local>
2025-09-05 16:40:34 +0400

    Fix image loading issue by disabling Next.js image optimization for Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // This is important for Cloudflare Pages
+  },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
